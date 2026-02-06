9890565f73ac97929f1d71f06aa7528c2f8aaba1
kuzminklk <kuzminkdk@gmail.com>
2026-02-06 19:07:07 +0300

    Add git commands from Gemini

--- technologies/git/common/commands.sh
+++ technologies/git/common/commands.sh
@@ -1,21 +1,21 @@
 
-Configuration
+# Configuration
 git config --global user.name 'kuzminklk' (?)
 git config --global user.email '...' (?)
 
-Initialization
+# Initialization
 git init project-1
 cd project-1 (?)
 git clone
 
-Make Commit
+# Make Commit
 git add *
 git commit -m "Message"
 git push
 
-Remote connection
+# Remote connection
 git remote -v
 git remote set-url origin git@github.com:kuzminklk/ai-page.git
 
-Change name of commit
+# Change name of commit
 git rebase -i HEAD~N
---
+++ technologies/git/gemini/commands.sh
@@ -0,0 +1,3 @@
+
+# Clean
+git clean -fdx
