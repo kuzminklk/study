a36f53a8ebff0eda70ab64e42132f980b0221862
kuzminklk <kuzminkdk@gmail.com>
2026-02-06 19:17:41 +0300

    Add git commands from Gemini

--- technologies/git/gemini/commands.sh
+++ technologies/git/gemini/commands.sh
@@ -1,3 +1,13 @@
 
 # Clean
 git clean -fdx
+
+# Rename branch
+# Currently on the branch
+git branch -m <new-name>
+#On a different branch
+git branch -m <old-name> <new-name>
+#Push new name to remote
+git push origin -u <new-name>
+#Remove old name from remote
+git push origin --delete <old-name>
