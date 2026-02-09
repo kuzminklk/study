701aa716d8e5a22dcd2381a5a5872cc77160b816
kuzminklk <kuzminkdk@gmail.com>
2026-02-09 18:29:57 +0300

    Rename files

--- technologies/web-3/syfrin-updraft/foundry-fundamentals/commands.sh
+++ technologies/web-3/syfrin-updraft/foundry-fundamentals/commands.sh
@@ -3,15 +3,17 @@
 
 # Anvil commands
 
-anvil
+anvil # Turn on local blockchain
 
 
 
 # Forge commands
 
+# Init workspace
 forge init
 
 forge compile 
+# Same as
 forge build
 
 forge test
@@ -20,7 +22,6 @@ forge test
 forge create StorageFactory --interactive --broadcast
 
 # Delpoy contract via script
-# «sepolia» variable in foundry.toml
 forge script script/DeployStorageFactory.s.sol --rpc-url sepolia --broadcast --account development
 
 # Formatting
@@ -30,8 +31,11 @@ forge fmt
 
 # Cast commands
 
+# Send write transaction
 cast send 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 "createStorageContract()" --rpc-url $RPC_URL
 
+# Send read transaction
 cast call 0x5FbDB2315678afecb367f032d93F642f64180aa3 "listsOfStorageContracts(uint256)" 1
 
+# Turn hex into decimal
 cast to-base 0x5747a dec
