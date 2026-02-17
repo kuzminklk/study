5ca26da158f880a17ce2b6b6b3bca704e5f44cca
kuzminklk <kuzminkdk@gmail.com>
2026-02-17 15:52:37 +0300

    Start study TypeScript from Dave Gray course

---
+++ programming-languages/typescript/dave-gray/basics/build/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "Daniel";
+let h1 = document.querySelector("h1");
+if (h1) {
+    h1.innerText = userName;
+}
+//# sourceMappingURL=main.js.map
---
+++ programming-languages/typescript/dave-gray/basics/src/main.ts
@@ -0,0 +1,7 @@
+
+let userName = "Daniel";
+
+let h1 = document.querySelector("h1");
+if (h1) {
+	h1.innerText = userName
+}
